ef77c620ad4dcc8c2d0f2e67691c036ab670b457
LF <958881587@qq.com>
2024-07-22 18:24:22 +0800

    feat: 常规代码优化

--- src/vdom/modules/attrs.js
+++ src/vdom/modules/attrs.js
@@ -1,29 +1,69 @@
-var booleanAttrs = ["allowfullscreen", "async", "autofocus", "autoplay", "checked", "compact", "controls", "declare",
-                "default", "defaultchecked", "defaultmuted", "defaultselected", "defer", "disabled", "draggable",
-                "enabled", "formnovalidate", "hidden", "indeterminate", "inert", "ismap", "itemscope", "loop", "multiple",
-                "muted", "nohref", "noresize", "noshade", "novalidate", "nowrap", "open", "pauseonexit", "readonly",
-                "required", "reversed", "scoped", "seamless", "selected", "sortable", "spellcheck", "translate",
-                "truespeed", "typemustmatch", "visible"]
+const booleanAttrs = [
+  "allowfullscreen",
+  "async",
+  "autofocus",
+  "autoplay",
+  "checked",
+  "compact",
+  "controls",
+  "declare",
+  "default",
+  "defaultchecked",
+  "defaultmuted",
+  "defaultselected",
+  "defer",
+  "disabled",
+  "draggable",
+  "enabled",
+  "formnovalidate",
+  "hidden",
+  "indeterminate",
+  "inert",
+  "ismap",
+  "itemscope",
+  "loop",
+  "multiple",
+  "muted",
+  "nohref",
+  "noresize",
+  "noshade",
+  "novalidate",
+  "nowrap",
+  "open",
+  "pauseonexit",
+  "readonly",
+  "required",
+  "reversed",
+  "scoped",
+  "seamless",
+  "selected",
+  "sortable",
+  "spellcheck",
+  "translate",
+  "truespeed",
+  "typemustmatch",
+  "visible",
+];
 
-var booleanAttrsDict = {}
-for(var i=0, len = booleanAttrs.length; i < len; i++) {
-  booleanAttrsDict[booleanAttrs[i]] = true
+const booleanAttrsDict = {};
+for (let i = 0, len = booleanAttrs.length; i < len; i++) {
+  booleanAttrsDict[booleanAttrs[i]] = true;
 }
 
 function updateAttrs(oldVnode, vnode) {
-  var key, cur, old, elm = vnode.elm,
-      oldAttrs = oldVnode.data.attrs || {}, attrs = vnode.data.attrs || {}
+  let key, cur, old;
+  const elm = vnode.elm;
+  const oldAttrs = oldVnode.data.attrs || {};
+  const attrs = vnode.data.attrs || {};
 
   // update modified attributes, add new attributes
   for (key in attrs) {
-    cur = attrs[key]
-    old = oldAttrs[key]
+    cur = attrs[key];
+    old = oldAttrs[key];
     if (old !== cur) {
       // TODO: add support to namespaced attributes (setAttributeNS)
-      if(!cur && booleanAttrsDict[key])
-        elm.removeAttribute(key)
-      else
-        elm.setAttribute(key, cur)
+      if (!cur && booleanAttrsDict[key]) elm.removeAttribute(key);
+      else elm.setAttribute(key, cur);
     }
   }
   //remove removed attributes
@@ -31,12 +71,12 @@ function updateAttrs(oldVnode, vnode) {
   // the other option is to remove all attributes with value == undefined
   for (key in oldAttrs) {
     if (!(key in attrs)) {
-      elm.removeAttribute(key)
+      elm.removeAttribute(key);
     }
   }
 }
 
 export default {
   create: updateAttrs,
-  update: updateAttrs
-}
+  update: updateAttrs,
+};
--- src/vdom/modules/props.js
+++ src/vdom/modules/props.js
@@ -1,21 +1,24 @@
 function updateProps(oldVnode, vnode) {
-  var key, cur, old, elm = vnode.elm,
-      oldProps = oldVnode.data.props || {}, props = vnode.data.props || {}
+  let key, cur, old;
+  const elm = vnode.elm;
+  const oldProps = oldVnode.data.props || {};
+  const props = vnode.data.props || {};
+
   for (key in oldProps) {
     if (!props[key]) {
-      delete elm[key]
+      delete elm[key];
     }
   }
   for (key in props) {
-    cur = props[key]
-    old = oldProps[key]
-    if (old !== cur && (key !== 'value' || elm[key] !== cur)) {
-      elm[key] = cur
+    cur = props[key];
+    old = oldProps[key];
+    if (old !== cur && (key !== "value" || elm[key] !== cur)) {
+      elm[key] = cur;
     }
   }
 }
 
 export default {
   create: updateProps,
-  update: updateProps
-}
+  update: updateProps,
+};

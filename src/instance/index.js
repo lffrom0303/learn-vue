d1d27932dd7b805a473f3d19dac5fd7c1a39d31f
LF <958881587@qq.com>
2024-07-22 18:31:04 +0800

    feat: 将Component类写法换成构造函数写法

--- src/instance/index.js
+++ src/instance/index.js
@@ -2,46 +2,44 @@ import { isReserved, getOuterHTML } from "../util/index";
 import { compile } from "../compiler/index";
 import Watcher from "../observer/watcher";
 import { h, patch } from "../vdom";
-export default class Component {
-  constructor(options) {
-    // 初始化配置
-    this.$options = options;
-    this._data = options.data;
-    console.log("初始化配置options、data");
-    const el = (this._el = document.querySelector(options.el));
-    this._el.innerHTML = "";
-    console.log("初始化配置el、dom");
-    // 解析DOM
-    let dom = getOuterHTML(el);
-    console.log("通过getOuterHTML方法，得到根节点字符串", dom);
-    const render = compile(dom);
-    console.log("得到了生成新建根节点html结构函数", render);
-    // 处理数据
-    Object.keys(options.data).forEach((key) => this._proxy(key));
-    console.log("处理数据完毕");
-    // 处理函数
-    if (options.methods) {
-      Object.keys(options.methods).forEach((key) => {
-        this[key] = options.methods[key].bind(this);
-      });
-    }
-    console.log("处理函数完毕");
-    // 创建监听者数组
-    this._watchers = [];
-    console.log("创建监听者数组", this._watchers);
-    // 创建一个监听者
-    console.log(
-      "创建一个监听者Watcher(传入当前组件实例this, 生成新建根节点dom结构函数, 更新函数cb, options) {}"
-    );
-    this._watcher = new Watcher(this, render, this._update);
-    console.log("7、创建一个监听者完毕", "创建监听者数组=>", this._watchers);
-    console.log(
-      "初始化执行一次this._update,传入this._watcher.value",
-      this._watcher.value
-    );
-    this._update(this._watcher.value);
-    console.log("更新后的vtree", this._tree);
+export default function Component(options) {
+  // 初始化配置
+  this.$options = options;
+  this._data = options.data;
+  console.log("初始化配置options、data");
+  const el = (this._el = document.querySelector(options.el));
+  this._el.innerHTML = "";
+  console.log("初始化配置el、dom");
+  // 解析DOM
+  let dom = getOuterHTML(el);
+  console.log("通过getOuterHTML方法，得到根节点字符串", dom);
+  const render = compile(dom);
+  console.log("得到了生成新建根节点html结构函数", render);
+  // 处理数据
+  Object.keys(options.data).forEach((key) => _proxy(this, key));
+  console.log("处理数据完毕");
+  // 处理函数
+  if (options.methods) {
+    Object.keys(options.methods).forEach((key) => {
+      this[key] = options.methods[key].bind(this);
+    });
   }
+  console.log("处理函数完毕");
+  // 创建监听者数组
+  this._watchers = [];
+  console.log("创建监听者数组", this._watchers);
+  // 创建一个监听者
+  console.log(
+    "创建一个监听者Watcher(传入当前组件实例this, 生成新建根节点dom结构函数, 更新函数cb, options) {}"
+  );
+  this._watcher = new Watcher(this, render, this._update);
+  console.log("7、创建一个监听者完毕", "创建监听者数组=>", this._watchers);
+  console.log(
+    "初始化执行一次this._update,传入this._watcher.value",
+    this._watcher.value
+  );
+  this._update(this._watcher.value);
+  console.log("更新后的vtree", this._tree);
   /**
    * 更新虚拟树。
    *
@@ -51,39 +49,38 @@ export default class Component {
    *
    * @param {Object} vtree 新的虚拟树对象，代表了应该呈现的DOM结构。
    */
-  _update(vtree) {
-    console.log("vtree", vtree);
-    // 检查当前是否有虚拟树，如果没有，则直接将新虚拟树应用于DOM
-    if (!this._tree) {
-      patch(this._el, vtree);
-    } else {
-      // 如果已经有虚拟树，则对当前虚拟树和新虚拟树进行比较并应用更新
-      patch(this._tree, vtree);
-    }
-    // 更新当前的虚拟树为新虚拟树，以便下次比较使用
-    this._tree = vtree;
+}
+Component.prototype._update = function (vtree) {
+  console.log("vtree", vtree);
+  // 检查当前是否有虚拟树，如果没有，则直接将新虚拟树应用于DOM
+  if (!this._tree) {
+    patch(this._el, vtree);
+  } else {
+    // 如果已经有虚拟树，则对当前虚拟树和新虚拟树进行比较并应用更新
+    patch(this._tree, vtree);
   }
+  // 更新当前的虚拟树为新虚拟树，以便下次比较使用
+  this._tree = vtree;
+};
 
-  _proxy(key) {
-    if (!isReserved(key)) {
-      // need to store ref to self here
-      // because these getter/setters might
-      // be called by child scopes via
-      // prototype inheritance.
-      var self = this;
-      Object.defineProperty(self, key, {
-        configurable: true,
-        enumerable: true,
-        get: function proxyGetter() {
-          console.log("proxyGetter");
-          return self._data[key];
-        },
-        set: function proxySetter(val) {
-          console.log("proxySetter", val);
-          self._data[key] = val;
-        },
-      });
-    }
+function _proxy(vm, key) {
+  if (!isReserved(key)) {
+    // need to store ref to self here
+    // because these getter/setters might
+    // be called by child scopes via
+    // prototype inheritance.
+    Object.defineProperty(vm, key, {
+      configurable: true,
+      enumerable: true,
+      get: function proxyGetter() {
+        console.log("proxyGetter");
+        return vm._data[key];
+      },
+      set: function proxySetter(val) {
+        console.log("proxySetter", val);
+        vm._data[key] = val;
+      },
+    });
   }
 }
 Component.prototype.__h__ = h;
